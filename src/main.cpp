e2f20deaa4030854998fb422854b58e0a659510b
mqc <0x0muu@gmail.com>
2023-12-27 17:38:01 +0900

    8 servos demo

--- src/main.cpp
+++ src/main.cpp
@@ -2,50 +2,78 @@
 #include <math.h>
 #include <Servo.h>
 
-class CustomServo {
+#define NUM_SERVOS 8
+
+struct ServoState {
+  float phaseshift; // 位相
+  float offset; // 振動の中心
+  float amplitude; // 振幅
+  float speed; // 回転速度
+};
+class Servo_ {
   private:
+    int pin;
     Servo servo;
-    float phaseshift; // フェーズシフト
-    float amplitude; // 振幅
+    ServoState state;
 
   public:
-    CustomServo(float phaseshift, float amplitude) {
-      this->phaseshift = phaseshift;
-      this->amplitude = amplitude;
+    Servo_(int pin, ServoState& state) : pin(pin), state(state){
+      // init();
     }
-    void attach(int pin){
+
+    void init(){
       servo.attach(pin);
     }
 
     float update(unsigned long t) {
-      float angle = amplitude * sin(t / 1000.0 + phaseshift);
-      servo.write(angle + 90);
+      float rad = state.phaseshift * M_PI / 180.0;
+      float angle = state.amplitude * sin(state.speed * t / 1000.0 + rad);
+      servo.write(angle + state.offset);
       return angle;
     }
 };
 
-CustomServo servo1(0, 60);
-CustomServo servo2(0, 30);
+
+/*
+servo positions
+    ----
+2 1 |  | 5 6 
+    |  |
+4 3 |  | 7 8
+    ----
+*/
+ServoState states[NUM_SERVOS] = {
+  {0, 90, 40, 2},
+  {0, 60, 15, 2},
+  {0, 90, 40, -2},
+  {0, 120, 15, -2},
+  {0, 90, 40, 2},
+  {0, 120, 15, 2},
+  {0, 90, 40, -2},
+  {0, 120, 15, -2}
+};
+Servo_ servos[NUM_SERVOS]{
+  Servo_(2, states[0]),
+  Servo_(3, states[1]),
+  Servo_(4, states[2]),
+  Servo_(5, states[3]),
+  Servo_(6, states[4]),
+  Servo_(7, states[5]),
+  Servo_(8, states[6]),
+  Servo_(9, states[7])
+};
 
 void setup() {
   Serial.begin(115200);
-  servo1.attach(A0);
-  servo2.attach(A1);
-  /*
-  for( int i=0; i<NUM_SERVOS; i++){
-    myServo[i].attach( A0 + i );      // pin A0,A1...
-    myServo[i].update(0);
-    delay(10);
+  for(int i=0; i<NUM_SERVOS; i++){
+    servos[i].init();
   }
-  */
 }
 
 void loop() {
-  //for( int i=0; i<NUM_SERVOS; i++){
-    unsigned long t = millis();
-    float angle = servo1.update(t);
-    servo2.update(t);
-    Serial.println(angle);
-    delay(200);
-  //}
+  unsigned long t = millis();
+  for(int i=0; i<NUM_SERVOS; i++){
+    servos[i].update(t);
+  }
+  delay(20);
 }
